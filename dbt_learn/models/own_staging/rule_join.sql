17064ee8c3a944291304f8e4d23e70270b51ee28
awaisajaz1 <awaisajaz1@gamil.com>
2022-11-10 12:46:27 +0500

    var in dbt_prject added

--- dbt_learn/models/own_staging/rule_join.sql
+++ dbt_learn/models/own_staging/rule_join.sql
@@ -3,7 +3,7 @@
 
 {{ config(schema='dbt_staging', materialized='table') }}
 
-select s.* from {{source('mysql', 'help_topic')}} as r
+select s.*, '{{ var("current_date") }}' as date from {{source('mysql', 'help_topic')}} as r
 left join 
 {{source('mysql', 'help_category')}} as s on r.help_category_id = s.help_category_id
 where s.help_category_id = 3
